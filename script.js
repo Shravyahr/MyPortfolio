2fcb84f40bef889cd0a3d477dcb285560415866c
SHRAVYA <shravyahr24@gmail.com>
2024-11-23 16:26:07 +0530

    Uploading all the Files

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple form submission handler
+document.querySelector('form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Thank you for reaching out! I will get back to you soon.');
+});
